99a4a3ecc6f23991f0f39db2b8476aaf4439b08e
abarabonova <abarabonovad@gmail.com>
2020-10-16 14:16:42 +0300

    link stages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :posts
-  get 'about/index'
+  # get 'about/index'
   get 'posts/index'
 
-  root 'posts#index'
+  # root 'posts#index'
+
+  get 'welcome', to: 'welcome#index'
+  get 'about', to: 'about#index'
+
+  root 'welcome#index'
+  # get 'welcome/index', to: 'welcome#index'
 end
