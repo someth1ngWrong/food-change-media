14de9f0df3e25ea7eeacff67b3f6f133d1c657f6
abarabonova <abarabonovad@gmail.com>
2020-12-17 13:55:48 +0300

    адаптив лендинга

---
+++ app/javascript/custom/index.js
@@ -0,0 +1,69 @@
+console.log(1)
+
+
+
+// let controller = new ScrollMagic.Controller()
+//
+// let gears = document.querySelector('.section__three__img')
+// let gearsAnimation = anime({
+//   targets: gears,
+//   rotate: '+=2turn',
+//   easing: 'linear',
+//   autoplay: false
+// })
+// let section__three = document.querySelector('.section__three')
+// new ScrollMagic.Scene({
+//   triggerElement: section__three,
+//   duration: section__three.getBoundingClientRect().height
+// })
+// .addTo(controller)
+// .on('progress', e => {
+//   gearsAnimation.seek(gearsAnimation.duration * e.progress)
+// })
+
+
+
+
+
+// 
+//
+// var leftgear = document.querySelector('.section__three__img'),
+//
+// window.addEventListener("scroll", function() {
+//     leftgear.style.transform = "20(deg)"+window.pageYOffset+"20deg)";
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  
   resources :posts
   resources :subscribers
   # get 'about/index'
-  get 'posts/index'
-
+  # get 'welcome/index', to: 'welcome#index'
+  # root 'welcome#index'
   # root 'posts#index'
+  get 'partners/index'
+  get 'landing/index'
 
+  get 'posts/index'
   get 'welcome', to: 'welcome#index'
   get 'about', to: 'about#index'
 
-  root 'welcome#index'
-  # get 'welcome/index', to: 'welcome#index'
+  root 'landing#index'
 end
