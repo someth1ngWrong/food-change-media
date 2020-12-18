900316dfd9db63f43abc2ea17270df400486fc3c
abarabonova <abarabonovad@gmail.com>
2020-12-18 17:21:23 +0300

    сброшенные стили скафолдом

--- db/schema.rb
+++ db/schema.rb
@@ -20,16 +20,6 @@ ActiveRecord::Schema.define(version: 2020_12_18_021546) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string "data_file_name", null: false
-    t.string "data_content_type"
-    t.integer "data_file_size"
-    t.string "type", limit: 30
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["type"], name: "index_ckeditor_assets_on_type"
-  end
-
   create_table "like_defs", force: :cascade do |t|
     t.integer "post_id", null: false
     t.integer "user_id", null: false
