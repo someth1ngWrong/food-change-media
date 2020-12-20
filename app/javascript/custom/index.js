bbf9790fd18cb21dc814d70055da2dd99756d6aa
abarabonova <abarabonovad@gmail.com>
2020-12-20 17:58:52 +0300

    стили для админки, постов и категорий

--- app/javascript/custom/index.js
+++ app/javascript/custom/index.js
@@ -1,37 +1,28 @@
 console.log(1)
 
+import ScrollMagic from 'ScrollMagic'
 
 
 let controller = new ScrollMagic.Controller()
 
-// let gears = document.querySelector('.section__three__img')
-// let gearsAnimation = anime({
-//   targets: gears,
-//   rotate: '+=2turn',
-//   easing: 'linear',
-//   autoplay: false
-// })
-// let section__three = document.querySelector('.section__three')
-// new ScrollMagic.Scene({
-//   triggerElement: section__three,
-//   duration: section__three.getBoundingClientRect().height
-// })
-// .addTo(controller)
-// .on('progress', e => {
-//   gearsAnimation.seek(gearsAnimation.duration * e.progress)
-// })
+let gears = document.querySelector('.section__three__img')
+let gearsAnimation = anime({
+  targets: gears,
+  rotate: '+=2turn',
+  easing: 'linear',
+  autoplay: false
+})
+let section__three = document.querySelector('.section__three')
+new ScrollMagic.Scene({
+  triggerElement: section__three,
+  duration: section__three.getBoundingClientRect().height
+})
+.addTo(controller)
+.on('progress', e => {
+  gearsAnimation.seek(gearsAnimation.duration * e.progress)
+})
 
 
-
-
-
-//
-//
-// var leftgear = document.querySelector('.section__three__img'),
-//
-// window.addEventListener("scroll", function() {
-//     leftgear.style.transform = "20(deg)"+window.pageYOffset+"20deg)";
-// });
 
 
 
