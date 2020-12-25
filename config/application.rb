5f13ff5d838bd4ea8c517447a50f9473537ba2dc
abarabonova <abarabonovad@gmail.com>
2020-12-25 19:17:56 +0300

    Подключила шрифты (ура?)

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ Bundler.require(*Rails.groups)
 
 module FoodChangeMedia
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.enabled = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
