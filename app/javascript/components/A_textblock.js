ad6c33c80d1ffa4fd42329b05dfc7feaebcb25a2
abarabonova <abarabonovad@gmail.com>
2021-03-23 18:30:24 +0300

    react add!!!

---
+++ app/javascript/components/A_textblock.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class A_textblock extends React.Component {
+  render () {
+    return (
+      <p>
+        A_textblock
+      </p>
+    );
+  }
+}
+
+export default A_textblock
